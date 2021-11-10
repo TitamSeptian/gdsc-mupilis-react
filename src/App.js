5a49f48a4891a5fac7421ad5fead8894311f62f6
TitamSeptian <dwiputra4456@protonmail.com>
2021-11-10 07:53:22 +0700

    add search function

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React, { useEffect,useState} from 'react';
-import { Row,Container, Col } from 'react-bootstrap';
+import { Row,Container, Col, InputGroup, FormControl, Button } from 'react-bootstrap';
 import Navbar from './components/myNavbar.js';
-import SerachBar from './components/searchMovie.js';
+// import SerachBar from './components/searchMovie.js';
 import MovieCard from './components/movieCard.js';
 
 function App() {
@@ -13,26 +13,66 @@ function App() {
   const fetchMovie = async () => {
     const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${api_key}`)
     const json = await data.json()
-    console.log(json.results);
+    setMovie(json.results);
+  }
+  const keyUpHandle = async (that) => {
+    console.log(that.keyCode)
+    if (that.keyCode == "13") {
+      let val = that.target.value
+      searchMovie(val)
+    }
+  }
+
+  const clickHandle = () => {
+    let element = document.getElementById("search-input");
+    searchMovie(element.value);
+  }
+  
+  const searchMovie = async (val) => {
+    const data = await fetch(`https://api.themoviedb.org/3/search/movie?api_key=${api_key}&query=${val}`)
+    const json = await data.json()
+    // console.log(json);
     setMovie(json.results);
   }
   return (
     <div>
       <Navbar />
-      <SerachBar />
+      <Container>
+        <Row className="mt-3 justify-content-center">
+          <Col md={8}>
+            <h1 className="text-center">Search Movie</h1>
+            <InputGroup className="mb-3">
+              <a href="/" onClick={fetchMovie} className="btn btn-dark" id="kembali">Back</a>
+              <FormControl
+                placeholder="Venomm"
+                onKeyUp={keyUpHandle.bind(this)}
+                id="search-input"
+              />
+              <Button variant="outline-success" onClick={clickHandle} id="search-button">
+                Search
+              </Button>
+            </InputGroup>
+          </Col>
+        </Row>
+      </Container>
       <Container>
         <Row id="movie-list">
-          {movies.map((movie,index) => (
-          <Col md={4}>
-            <MovieCard
-                src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
-                title={movie.original_title}
-                date={movie.release_date}
-                id={movie.id}
-                key={movie.id}
-                 />
-          </Col>
-          ))}
+          {/* <div id="movie-list"> */}
+            {movies.map((movie,index) => (
+            <Col md={4}>
+              <MovieCard
+                  src={"https://image.tmdb.org/t/p/w500" + movie.poster_path}
+                  title={movie.original_title}
+                  date={movie.release_date}
+                  id={movie.id}
+                  key={movie.id}
+                  rating={movie.vote_average}
+                  overview={movie.overview}
+                  lang={movie.original_language}
+                  />
+            </Col>
+            ))}
+          {/* </div>  */}
         </Row>
       </Container>
     </div>
--- src/components/movieCard.js
+++ src/components/movieCard.js
@@ -1,22 +1,52 @@
-import { Card } from "react-bootstrap";
+import React,{ useState } from "react";
+import { Card,Modal,Button, Row, Col } from "react-bootstrap";
 
-const movieCard = ({ src, title, date, rating,id }) => {
-    let url = "https://api.themoviedb.org/3/movie/"+id
+const MovieCard = ({ src, title, date, rating,id,overview,lang}) => {
+    let url = "https://api.themoviedb.org/3/movie/"+id;
+    const [show, setShow] = useState(false);
+    const handleClose = () => setShow(false);
+    const handleShow = () => setShow(true);
     return (
         <div>
             <Card>
                 <Card.Img variant="top" src={src} alt="Movie Poster"/>
                 <Card.Body>
                     <Card.Title>
-                        <h5>asdasd</h5>
+                        <h5>{title}</h5>
                     </Card.Title>
                     <Card.Subtitle className="mb-2 text-muted">{date} </Card.Subtitle>
                     <Card.Subtitle className="mb-2 text-muted">Rating : {rating} </Card.Subtitle>
-                    <a href="javascipt:void(0)" className="card-link see-detail2" data-id={id} data-url={ url}>See Details</a>
+                    <a href="javascript:void(0)" className="card-link see-detail2" data-id={id} onClick={handleShow} data-url={url}>See Details</a>
                 </Card.Body>
             </Card>
+            <Modal show={show} onHide={handleClose}>
+                <Modal.Header closeButton>
+                    <Modal.Title>{title}</Modal.Title>
+                </Modal.Header>
+                <Modal.Body>
+                    <Row>
+                        <Col md={4}>
+                            <img src={src} className="img-fluid" />
+                        </Col>
+                        <Col md={8}>
+                            <ul className="list-group">
+                                <li className="list-group-item"><h3>{title}</h3></li>
+                                <li className="list-group-item">Released : {date}</li>
+                                <li className="list-group-item">Language : {lang}</li>
+                                <li className="list-group-item">Rating : {rating}</li>
+                                <li className="list-group-item">Overview : {overview}</li>
+                            </ul>
+                        </Col>
+                    </Row>
+                </Modal.Body>
+                <Modal.Footer>
+                    <Button variant="secondary" onClick={handleClose}>
+                        Close
+                    </Button>
+                </Modal.Footer>
+            </Modal>
         </div>
     );
 }
 
-export default movieCard;
+export default MovieCard;
